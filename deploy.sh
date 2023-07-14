12abe543875605fba366c457196ec60b66dcb9f3
Rino <cocorine999@gmail.com>
2023-07-14 16:31:10 +0200

    New test

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
+git branch -M main
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com/cocorine999/hokwe-landing.git main:gh-pages
